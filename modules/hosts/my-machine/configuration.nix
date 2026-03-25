b4b7ada37b99ed3f397cbae6acdc70c45cc13453
max <maxmiller1204@outlook.com>
2026-03-24 20:08:23 -0400

    fixed bootloader clutter

--- modules/hosts/my-machine/configuration.nix
+++ modules/hosts/my-machine/configuration.nix
@@ -47,6 +47,7 @@
 
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 5;
     boot.loader.efi.canTouchEfiVariables = true;
 
     # Use latest kernel.
