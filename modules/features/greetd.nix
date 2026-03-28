a9e8ceea1fb87366bfcd5209ba27b98a05d53ffe
Max <maxmiller1204@outlook.com>
2026-03-27 20:48:05 -0400

    added regreet

--- modules/features/greetd.nix
+++ modules/features/greetd.nix
@@ -1,15 +1,11 @@
 { self, inputs, ... }: {
   flake.nixosModules.greetd = { pkgs, lib, config, ... }: {
-    options.features.greetd.enable = lib.mkEnableOption "greetd display manager with tuigreet";
+    options.features.greetd.enable = lib.mkEnableOption "greetd display manager with regreet";
 
     config = lib.mkIf config.features.greetd.enable {
-      services.greetd = {
+      programs.regreet = {
         enable = true;
-        useTextGreeter = true;
-        settings.default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
-          user = "greeter";
-        };
+        settings.default_session.command = "niri-session";
       };
     };
   };
