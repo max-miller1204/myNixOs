bb1fae0b6b59e1f97c2cb7704a3e66c3878c0f58
Max <maxmiller1204@outlook.com>
2026-04-02 09:31:32 -0400

    fixed omx again

--- modules/aspects/omx-activate.nix
+++ modules/aspects/omx-activate.nix
@@ -5,8 +5,7 @@
       system.activationScripts.omx-activate = {
         text = ''
           if [ -x "${pkgs.oh-my-codex}/bin/omx-activate" ]; then
-            ${pkgs.sudo}/bin/sudo -u max \
-              HOME=/home/max \
+            ${pkgs.sudo}/bin/sudo -u max -i \
               PATH=${pkgs.bash}/bin:${pkgs.coreutils}/bin:${pkgs.findutils}/bin:${pkgs.nodejs}/bin:$PATH \
               ${pkgs.oh-my-codex}/bin/omx-activate || true
           fi
